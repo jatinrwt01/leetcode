ada87313a18a70ed23b092948351228827f780db
Jatin Rawat <work.jatinynk@gmail.com>
2026-06-28 07:42:28 +0530

    Time: 0 ms (100%), Space: 55.2 MB (69.21%) - LeetHub - Time: O(n log n), Space: O(1)

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -3,14 +3,11 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         int n = arr.size();
         sort(arr.begin(), arr.end());
-        if(arr[0] != 1){
-            arr[0] = 1;
-        }
-        for(int i=1; i<n; i++){
-            if(abs(arr[i]-arr[i-1]) <= 1) continue;
-            if(abs(arr[i] - arr[i-1]) > 1){
-                arr[i] = arr[i-1] + 1;
-            }
+        if(arr[0]> 1){arr[0] = 1;}
+        int i = 1;
+        while(i<n){
+            abs(arr[i] - arr[i-1]) <= 1 ? arr[i] = arr[i] : arr[i] = arr[i-1]+1;
+            i++;
         }
        return arr[n-1];
     }
