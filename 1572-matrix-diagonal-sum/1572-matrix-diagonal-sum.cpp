ca6510ec0d54588785c1819d7dc7d420fe5431c5
Jatin Rawat <work.jatinynk@gmail.com>
2026-06-28 11:26:25 +0530

    Time: 0 ms (100%), Space: 14.8 MB (99.28%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int s = 0;
+        for(int i=0; i<n; i++){
+            s+=mat[i][i];
+        }
+        for(int i=0; i<n; i++){
+            s+=mat[i][n-i-1];
+        }
+        if(n%2 != 0){
+            s-=mat[n/2][n/2];
+        }
+        return s;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
