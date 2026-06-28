5fe8e1aa26e95c46670b500b07fbe640c6b1ebbd
Jatin Rawat <work.jatinynk@gmail.com>
2026-06-28 07:31:27 +0530

    Time: 3 ms (94.63%), Space: 55.1 MB (69.21%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        if(arr[0] != 1){
+            arr[0] = 1;
+        }
+        for(int i=1; i<n; i++){
+            if(abs(arr[i]-arr[i-1]) <= 1) continue;
+            if(abs(arr[i] - arr[i-1]) > 1){
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+        int ans = INT_MIN;
+        for(int a : arr){
+            ans = max(a,ans);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
