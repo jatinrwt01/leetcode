bab20e4e04265ad750ad9f8433c0af591d2d7e7d
Jatin Rawat <work.jatinynk@gmail.com>
2026-06-29 10:39:16 +0530

    Time: 0 ms (100%), Space: 14.9 MB (93.91%) - LeetHub - Time: O(m*n), Space: O(m*n)

---
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
