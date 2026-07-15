ef32ad02769a7aa4e5f6c65de63552d180cf74c9
Jatin Rawat <work.jatinynk@gmail.com>
2026-07-15 21:02:17 +0530

    solve p/np pattern dp

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,14 @@
 class Solution {
-private:
-    int f(int i, vector<int>&nums, vector<int> &dp){
-        if(i == 0) return nums[0];
-        if(i<0) return 0;
-        if(dp[i]!=INT_MIN){
-            return dp[i];
-        }
-        int p = nums[i]+f(i-2, nums, dp);
-        int np = 0 + f(i-1, nums, dp);
-        return dp[i] = max(p,np);
-    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1, INT_MIN);
-        return f(n-1, nums, dp);
+        vector<int>dp(n, INT_MIN);
+        dp[0] = nums[0];
+        int neg=0;
+        for(int i=1; i<n; i++){
+            dp[i] = max(nums[i]+ ((i>1) ? dp[i-2] : neg) ,dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
 
