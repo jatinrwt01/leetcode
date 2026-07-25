d817cf613a54189cc8649a7dd2f4f9fa22512283
Jatin Rawat <work.jatinynk@gmail.com>
2026-07-25 20:25:09 +0530

    solve 2d dp tabulation

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,19 @@
 class Solution {
-private:
-    int f(int r, int c, int m, int n, vector<vector<int>>&dp){
-        if(r == 0 && c == 0){
-            return 1;
-        }
-        if(dp[r][c] != -1){
-            return dp[r][c];
-        }
-        int right = 0;
-        if(c-1>=0){
-           right = f(r, c-1, m, n, dp);
-        }
-        int down = 0;
-        if(r-1>=0){
-           down = f(r-1, c,m ,n, dp);
-        }
-        return dp[r][c] = right+down;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
-        return f(m-1, n-1, m, n, dp);
+        vector<vector<int>>dp(m, vector<int>(n, 0));
+        for(int i=0; i<m; i++){
+            dp[i][0] = 1;
+        }
+        for(int j=0; j<n; j++){
+            dp[0][j] = 1;
+        }
+        for(int r=1; r<m; r++){
+            for(int c=1; c<n; c++){
+                dp[r][c] = ((r-1>=0)? dp[r-1][c] : 0)+((c-1>=0)?dp[r][c-1] : 0);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
 
