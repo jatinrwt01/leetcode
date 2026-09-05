09e41539eee7cbc42baf7f61ec497cc03329abcc
Jatin Rawat <work.jatinynk@gmail.com>
2026-09-05 08:32:26 +0530

    optimize NGE using monotonic stack

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-      int n1=nums1.size(), n2=nums2.size();
-      vector<int>ans(n1,0);
-      for(int i=0; i<n1; i++){
-        int nge=-1, j=0;
-        while(j<n2 && nums1[i] != nums2[j]){
-            j++;
+        int n1=nums1.size(), n2=nums2.size();
+        unordered_map<int,int>mp;
+        stack<int>st;
+        vector<int>ans(n1,0);
+        mp[nums2[n2-1]]=-1;
+        for(int i=n2-1; i>=0; i--){
+            if(!st.empty() && st.top()<nums2[i]){
+                while(!st.empty() && st.top()<nums2[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                mp[nums2[i]]=-1;
+                }else{
+                    mp[nums2[i]]=st.top();
+                }
+            } else if(!st.empty() && st.top()>nums2[i]){
+                mp[nums2[i]]=st.top();
+            }
+            st.push(nums2[i]);
         }
-        for(int k=j+1; k<n2; k++){
-        if(nums2[k]>nums2[j]){
-            nge=nums2[k];
-            break;
+
+        for(int i=0; i<n1; i++){
+            ans[i]=mp[nums1[i]];
         }
-        }
-        ans[i]=nge;
-      }
-       return ans;
+        return ans;
     }
 };
 
