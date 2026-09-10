d516225ccdcfd33722f38708c3049b7d1840c1a6
Jatin Rawat <work.jatinynk@gmail.com>
2026-09-10 08:24:11 +0530

    solve height of BT

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int height(TreeNode* node){
+        if(node == NULL){
+            return 0;
+        }
+        int lh=1+height(node->left);
+        int rh= 1+height(node->right);
+        return max(lh, rh);
+    }
+public:
+    int maxDepth(TreeNode* root) {
+        return height(root);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
