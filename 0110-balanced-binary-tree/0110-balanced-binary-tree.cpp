6eaa23c6642dd5455f6206b5ed0bc2e9cd325adf
Jatin Rawat <work.jatinynk@gmail.com>
2026-09-11 09:00:17 +0530

    solve check balanced BT

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int solve(TreeNode* node){
+        if(node==NULL){
+            return 0;
+        }
+        int lh = solve(node->left);
+        if(lh==-1){
+            return -1;
+        }
+        int rh = solve(node->right);
+        if(rh == -1){
+            return -1;
+        }
+        if(abs(lh-rh)>1){
+            return -1;
+        }
+        return 1+max(lh,rh);
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL){
+            return true;
+        }
+        return (solve(root)!=-1);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
