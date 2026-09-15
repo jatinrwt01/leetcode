22b88d04d4ca515c236503c49f4994789336427a
Jatin Rawat <work.jatinynk@gmail.com>
2026-09-15 08:07:41 +0530

    tabulate daily question 15 sept

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -10,27 +10,20 @@ private:
         }
         return true;
     }
-    int helper(int i,string &s, int k, vector<int>&dp){
-        if(i<0){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int ti=INT_MIN, dti=INT_MIN;
-        dti=helper(i-1, s, k,dp);
-        for(int j=0; j<=i; j++){
-            if((i-j+1)>=k && isPalindrome(s, j, i)){
-                ti=max(ti, 1+helper(j-1, s, k,dp));
-            }
-        }
-        return dp[i]=max(ti, dti);
-    }
 public:
     int maxPalindromes(string s, int k) {
         int n=s.size();
         vector<int>dp(n+1, -1);
-        return helper(n-1, s, k,dp);
+        dp[0]=0;
+        for(int i=0; i<n; i++){
+            dp[i+1]=dp[i];
+            for(int j=0; j<=i; j++){
+            if((i-j+1)>=k && isPalindrome(s, j, i)){
+                dp[i+1]=max(dp[i+1], 1+dp[j]);
+            }
+        }
+        }
+        return dp[n];
     }
 };
 
